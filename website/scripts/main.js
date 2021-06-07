fb94a278745eff4437f66c7fcd160b39b74274c0
yasasgit <yasas99@outlook.com>
2021-06-07 17:49:30 +0530

    nav

--- website/scripts/main.js
+++ website/scripts/main.js
@@ -58,9 +58,25 @@ function currentSlide(index) {
 
 function responsive() {
     let x = document.getElementById("nav");
-    if (x.className === "nav") {
-        x.className += " responsive";
+    if (x.className === "navi") {
+        x.className += "-responsive";
     } else {
-        x.className = "nav";
+        x.className = "navi";
     }
 }
+function openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
