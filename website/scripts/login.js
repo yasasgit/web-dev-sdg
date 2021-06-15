0c09965e65a76cae822e45dcdc796fb09e1c3782
unknown <wadkavinda@students.nsbm.ac.lk>
2021-06-15 20:10:42 +0530

    update login.js

---
+++ website/scripts/login.js
@@ -0,0 +1,25 @@
+function checkForm()
+ {
+var email = document.form.email.value;
+var password = document.form.password.value;
+
+// validation fails if the input is blank
+    if( email== ""|| password== "") 
+    {
+    	alert("Error: Input is empty!");
+    	return false;
+    }
+// validate email 
+	if (!email.includes("@",".")) 
+	{
+	    alert("Not a valid e-mail address");
+	    return false;
+    }   
+
+//incorrect password
+    //if (password != confirmpassword)
+    {
+        alert("incorrect password");
+        return false;
+    }
+}
