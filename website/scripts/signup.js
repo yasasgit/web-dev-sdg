353c9896bcff77549a501df6d52563cda7187c7f
unknown <wadkavinda@students.nsbm.ac.lk>
2021-06-17 22:14:47 +0530

    update signup.js

---
+++ website/scripts/signup.js
@@ -0,0 +1,13 @@
+function checkForm()
+ {
+ 	var firstname = document.form.firstname.value;
+ 	var lastname = document.form.lastname.value;
+ 	var date = document.form.date.value;
+ 	var month = document.form.month.value;
+ 	var year = document.form.value;
+ 	var gender = document.form.gender.value;
+ 	var country = document.form.country.value;
+ 	var email = document.form.email.value;
+ 	var phone = document.form.phone.value;
+ 	var password = document.form.password.value;
+ 	var confirmpassword = document.form.confirmpassword.value;
