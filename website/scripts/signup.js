b092b381d267f6f87386051fe919e7b6012aca73
unknown <wadkavinda@students.nsbm.ac.lk>
2021-06-18 14:21:59 +0530

    update signup.js

--- website/scripts/signup.js
+++ website/scripts/signup.js
@@ -11,3 +11,41 @@ function checkForm()
  	var phone = document.form.phone.value;
  	var password = document.form.password.value;
  	var confirmpassword = document.form.confirmpassword.value;
+
+
+    // validation fails if the input is blank
+    if(firstname == "" || lastname== "" || gender== "" || email== "" || phone== "" || password== "" || confirmpassword== "" ) 
+    {
+    	alert("Error: Input is empty!");
+    	return false;
+    }
+
+    // validation fails if the input is blank
+    if(date == "Date" || month == "Month" || year == "Year" || country == "Country")
+    {
+    	alert("Error: Input is empty!");
+    	return false;
+    }
+
+    // validate email 
+	if (!email.includes("@",".")) 
+	{
+	    alert("Not a valid e-mail address");
+	    return false;
+    }   
+
+	// validate phone number
+	if (isNaN(phone) || phone.length != 10) 
+	{
+	    alert("Not a valid phone number");
+	    return false;
+    }
+
+    //validate password confirmation
+    if (password != confirmpassword)
+    {
+        alert("Pasword confirmation failed");
+        return false;
+    }
+
+}
