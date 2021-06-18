26d717cc760dc4dea7c3d976d4d460f5a224bef0
yasasgit <yasas99@outlook.com>
2021-06-19 00:26:36 +0530

    small fixes

--- website/scripts/login.js
+++ website/scripts/login.js
@@ -1,20 +1,17 @@
-function checkForm()
- {
-var email = document.form.email.value;
-var password = document.form.password.value;
+function checkForm() {
+    var email = document.form.email.value;
+    var password = document.form.password.value;
 
 // validation fails if the input is blank
-    if( email== ""|| password== "") 
-    {
-    	alert("Error: Input is empty!");
-    	return false;
+    if (email === "" || password === "") {
+        alert("Error: Input is empty!");
+        return false;
     }
 // validate email 
-	if (!email.includes("@",".")) 
-	{
-	    alert("Not a valid e-mail address");
-	    return false;
-    }   
+    if (!email.includes("@", ".")) {
+        alert("Not a valid e-mail address");
+        return false;
+    }
 
 //incorrect password
     //if (password != confirmpassword)
--- website/scripts/signup.js
+++ website/scripts/signup.js
@@ -1,49 +1,43 @@
-function checkForm()
- {
- 	var firstname = document.form.firstname.value;
- 	var lastname = document.form.lastname.value;
- 	var date = document.form.date.value;
- 	var month = document.form.month.value;
- 	var year = document.form.value;
- 	var gender = document.form.gender.value;
- 	var country = document.form.country.value;
- 	var email = document.form.email.value;
- 	var phone = document.form.phone.value;
- 	var password = document.form.password.value;
- 	var confirmpassword = document.form.confirmpassword.value;
+function checkForm() {
+    var firstname = document.form.firstname.value;
+    var lastname = document.form.lastname.value;
+    var date = document.form.date.value;
+    var month = document.form.month.value;
+    var year = document.form.value;
+    var gender = document.form.gender.value;
+    var country = document.form.country.value;
+    var email = document.form.email.value;
+    var phone = document.form.phone.value;
+    var password = document.form.password.value;
+    var confirmpassword = document.form.confirmpassword.value;
 
 
     // validation fails if the input is blank
-    if(firstname == "" || lastname== "" || gender== "" || email== "" || phone== "" || password== "" || confirmpassword== "" ) 
-    {
-    	alert("Error: Input is empty!");
-    	return false;
+    if (firstname === "" || lastname === "" || gender === "" || email === "" || phone === "" || password === "" || confirmpassword === "") {
+        alert("Error: Input is empty!");
+        return false;
     }
 
     // validation fails if the input is blank
-    if(date == "Date" || month == "Month" || year == "Year" || country == "Country")
-    {
-    	alert("Error: Input is empty!");
-    	return false;
+    if (date === "Date" || month === "Month" || year === "Year" || country === "Country") {
+        alert("Error: Input is empty!");
+        return false;
     }
 
     // validate email 
-	if (!email.includes("@",".")) 
-	{
-	    alert("Not a valid e-mail address");
-	    return false;
-    }   
+    if (!email.includes("@", ".")) {
+        alert("Not a valid e-mail address");
+        return false;
+    }
 
-	// validate phone number
-	if (isNaN(phone) || phone.length != 10) 
-	{
-	    alert("Not a valid phone number");
-	    return false;
+    // validate phone number
+    if (isNaN(phone) || phone.length !== 10) {
+        alert("Not a valid phone number");
+        return false;
     }
 
     //validate password confirmation
-    if (password != confirmpassword)
-    {
+    if (password !== confirmpassword) {
         alert("Pasword confirmation failed");
         return false;
     }
