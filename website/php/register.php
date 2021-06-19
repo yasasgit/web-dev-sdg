e1d5f7be8b11e6bd11b2511ba5f24bd1e9101a31
unknown <wadkavinda@students.nsbm.ac.lk>
2021-06-19 21:04:31 +0530

    update register.php

--- website/php/register.php
+++ website/php/register.php
@@ -132,5 +132,258 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         <p>Already have an account? <a href="login.php">Login here</a>.</p>
     </form>
 </div>
+<!DOCTYPE html>
+<html dir="ltr" lang="en">
+<head>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <title>Signup</title>
+    <link href="../css/normalize.css" rel="stylesheet">
+    <!--Framework -->
+    <link href="../css/master.css" rel="stylesheet">
+    <!--Nav and Footer CSS -->
+    <link href="../css/signup.css" rel="stylesheet">
+    <!--Page CSS -->
+    <link href="../media/gov-logo-ico.png" rel="icon">
+    <script src="../scripts/signup.js"></script>
+</head>
+<body>
+<header>
+    <div class="cover"></div>
+    <div class="about">
+        <h2>Sign up</h2>
+    </div>
+    <div class="gt5">
+        <form class="box" method="post" name="form" onsubmit="checkForm()">
+            <table>
+                <tr>
+                    <td>First Name</td>
+                    <td><input name="firstname" placeholder="Enter First Name" type="text"></td>
+                </tr>
+                <tr>
+                    <td>Last Name</td>
+                    <td><input name="lastname" placeholder="Enter Last Name" type="text"></td>
+                </tr>
+                <tr>
+                    <td>Date of Birth</td>
+                    <td><select name="date">
+                        <option>Date</option>
+                        <option>1</option>
+                        <option>2</option>
+                        <option>3</option>
+                        <option>4</option>
+                        <option>5</option>
+                        <option>6</option>
+                        <option>7</option>
+                        <option>8</option>
+                        <option>9</option>
+                        <option>10</option>
+                        <option>11</option>
+                        <option>12</option>
+                        <option>13</option>
+                        <option>14</option>
+                        <option>15</option>
+                        <option>16</option>
+                        <option>17</option>
+                        <option>18</option>
+                        <option>19</option>
+                        <option>20</option>
+                        <option>21</option>
+                        <option>22</option>
+                        <option>23</option>
+                        <option>24</option>
+                        <option>25</option>
+                        <option>26</option>
+                        <option>27</option>
+                        <option>28</option>
+                        <option>29</option>
+                        <option>30</option>
+                        <option>31</option>
+                    </select>
+                        <select name="month">
+                            <option>Month</option>
+                            <option>January</option>
+                            <option>February</option>
+                            <option>March</option>
+                            <option>April</option>
+                            <option>May</option>
+                            <option>June</option>
+                            <option>July</option>
+                            <option>August</option>
+                            <option>September</option>
+                            <option>October</option>
+                            <option>November</option>
+                            <option>December</option>
+                        </select>
+                        <select name="year">
+                            <option>Year</option>
+                            <option>1980</option>
+                            <option>1981</option>
+                            <option>1982</option>
+                            <option>1983</option>
+                            <option>1984</option>
+                            <option>1985</option>
+                            <option>1986</option>
+                            <option>1987</option>
+                            <option>1988</option>
+                            <option>1989</option>
+                            <option>1990</option>
+                            <option>1991</option>
+                            <option>1992</option>
+                            <option>1993</option>
+                            <option>1994</option>
+                            <option>1995</option>
+                            <option>1996</option>
+                            <option>1997</option>
+                            <option>1998</option>
+                            <option>1999</option>
+                            <option>2000</option>
+                            <option>2001</option>
+                            <option>2002</option>
+                            <option>2003</option>
+                            <option>2004</option>
+                            <option>2005</option>
+                            <option>2006</option>
+                            <option>2007</option>
+                            <option>2008</option>
+                            <option>2009</option>
+                            <option>2010</option>
+                            <option>2011</option>
+                            <option>2012</option>
+                            <option>2013</option>
+                            <option>2014</option>
+                            <option>2015</option>
+                            <option>2016</option>
+                            <option>2017</option>
+                            <option>2018</option>
+                            <option>2019</option>
+                            <option>2020</option>
+                        </select>
+                </tr>
+                <tr>
+                    <td class="box">Gender</td>
+                    <td><input name="gender" type="radio">Male
+                        <input name="gender" type="radio">Female
+                    </td>
+                </tr>
+                <tr>
+                    <td>Country</td>
+                    <td><select name="country">
+                        <option>Country</option>
+                        <option>Sri Lanka</option>
+                        <option>USA</option>
+                        <option>UK</option>
+                        <option>Japan</option>
+                    </select></td>
+                </tr>
+                <tr>
+                    <td>E-mail</td>
+                    <td><input name="email" placeholder="Enter E-mail" type="text"></td>
+                </tr>
+                <tr>
+                    <td>Phone</td>
+                    <td><input name="phone" placeholder="Enter Phone" type="text"></td>
+                </tr>
+                <tr>
+                    <td>Password</td>
+                    <td><input name="password" type="password"></td>
+                </tr>
+                <tr>
+                    <td>Confirm Password</td>
+                    <td><input name="confirmpassword" type="password"></td>
+                </tr>
+
+                <tr class="FirstLast">
+                    <td colspan="2">
+                        <input class="submit" type="Submit">
+                        <input class="reset" type="Reset" value="Cancel">
+                    </td>
+                </tr>
+            </table>
+        </form>
+    </div>
+</header>
+<footer>
+    <div class="row1">
+        <div class="col">
+            <h4>Explore</h4>
+            <a href="./invest.html">Why Should You Invest?</a>
+            <a href="./economy.html">Statistics</a>
+            <a href="./innovate.html">Innovation</a>
+            <a href="./startup.html">Startup</a>
+        </div>
+        <div class="col">
+            <h4>Quick Links</h4>
+            <a href="../index.html">Home</a>
+            <a href="./zone.html">Investor Zone</a>
+            <a href="./contact.html">Contact</a>
+            <a href="#">hfhdghd</a>
+        </div>
+        <div class="col">
+            <h4>Our Partners</h4>
+            <a href="https://investsrilanka.com">BOI</a>
+            <a href="https://www.ugc.ac.lk">UGC</a>
+            <a href="https://www.portcitycolombo.lk">Port City</a>
+            <a href="http://www.statistics.gov.lk">Statistics.lk</a>
+        </div>
+        <div class="col">
+            <h4>Follow</h4>
+            <a href="https://www.facebook.com">Facebook</a>
+            <a href="https://www.instagram.com">Instagram</a>
+            <a href="https://twitter.com">Twitter</a>
+            <a href="https://www.linkedin.com">LinkedIn</a>
+        </div>
+    </div>
+    <div class="row2">
+        <p>
+            Copyright &copy; 2021 All rights reserved.
+        </p>
+    </div>
+</footer>
+<div class="navi" id="nav">
+    <div class="left">
+        <div class="logo-li">
+            <a href="../index.html">
+                <img alt="" class="logo" src="../media/gov-logo.png">
+            </a>
+        </div>
+        <div class="menu-li">
+            <a href="javascript:void(0);" onclick="responsive()">
+                <img alt="" class="menu" src="../media/burger.png">
+            </a>
+        </div>
+    </div>
+    <div class="right">
+        <ul class="nav">
+            <li class="nav-li">
+                <a href="../html/invest.html">Why Should You Invest?</a>
+            </li>
+            <li class="nav-li">
+                <a href="../html/zone.html">Investor Zone</a>
+            </li>
+            <li class="nav-li">
+                <a href="../html/economy.html">Statistics</a>
+            </li>
+            <li class="nav-li">
+                <a href="../html/innovate.html">Innovation</a>
+            </li>
+            <li class="nav-li">
+                <a href="../html/startup.html">Startup</a>
+            </li>
+            <li class="nav-li">
+                <a href="../html/contact.html">Contact</a>
+            </li>
+        </ul>
+    </div>
+    <div class="gradient"></div>
+</div>
+<script src="../scripts/main.js" type="text/javascript">
+</script>
+<script type="text/javascript">
+    window.onscroll = function () {
+        hideNav();
+    }
+</script>
+</body>
+</html>
 </body>
 </html>
