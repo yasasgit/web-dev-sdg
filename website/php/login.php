9ad217ed657de75cd80394044bb176366493b61f
unknown <wadkavinda@students.nsbm.ac.lk>
2021-06-19 21:03:44 +0530

    update login.php

--- website/php/login.php
+++ website/php/login.php
@@ -127,5 +127,128 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             <p>Don't have an account? <a href="register.php">Sign up now</a>.</p>
         </form>
     </div>
+    <!DOCTYPE html>
+<html dir="ltr" lang="en">
+<head>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <title>Login</title>
+    <link href="../css/normalize.css" rel="stylesheet">
+    <!--Framework -->
+    <link href="../css/master.css" rel="stylesheet">
+    <!--Nav and Footer CSS -->
+    <link href="../css/login.css" rel="stylesheet">
+    <!--Page CSS -->
+    <link href="../media/gov-logo-ico.png" rel="icon">
+    <script src="../scripts/login.js"></script>
+</head>
+<body>
+<header>
+    <div class="cover"></div>
+    <div class="about">
+        <h2>Sign in to your Account</h2>
+    </div>
+    <div class="gt4">
+        <form class="box">
+            <label>
+                <input name="email" placeholder="Email" type="text">
+            </label>
+            <label>
+                <input name="password" placeholder="Password" type="password">
+            </label>
+            <label class="container">
+                <input type="checkbox">
+                <span class="checkmark"></span>
+                <a class="remember" href="#">Remember me</a>
+            </label>
+            <a class="forgot" href="#">Forgot Password?</a>
+            <input class="btn" type="submit" value="Sign in">
+
+        </form>
+        <a class="sign" href="../html/signup.html">Don’t have an account? Sign up</a>
+    </div>
+</header>
+<footer>
+    <div class="row1">
+        <div class="col">
+            <h4>Explore</h4>
+            <a href="./invest.html">Why Should You Invest?</a>
+            <a href="./economy.html">Statistics</a>
+            <a href="./innovate.html">Innovation</a>
+            <a href="./startup.html">Startup</a>
+        </div>
+        <div class="col">
+            <h4>Quick Links</h4>
+            <a href="../index.html">Home</a>
+            <a href="./zone.html">Investor Zone</a>
+            <a href="./contact.html">Contact</a>
+            <a href="#">hfhdghd</a>
+        </div>
+        <div class="col">
+            <h4>Our Partners</h4>
+            <a href="https://investsrilanka.com">BOI</a>
+            <a href="https://www.ugc.ac.lk">UGC</a>
+            <a href="https://www.portcitycolombo.lk">Port City</a>
+            <a href="http://www.statistics.gov.lk">Statistics.lk</a>
+        </div>
+        <div class="col">
+            <h4>Follow</h4>
+            <a href="https://www.facebook.com">Facebook</a>
+            <a href="https://www.instagram.com">Instagram</a>
+            <a href="https://twitter.com">Twitter</a>
+            <a href="https://www.linkedin.com">LinkedIn</a>
+        </div>
+    </div>
+    <div class="row2">
+        <p>
+            Copyright &copy; 2021 All rights reserved.
+        </p>
+    </div>
+</footer>
+<div class="navi" id="nav">
+    <div class="left">
+        <div class="logo-li">
+            <a href="../index.html">
+                <img alt="" class="logo" src="../media/gov-logo.png">
+            </a>
+        </div>
+        <div class="menu-li">
+            <a href="javascript:void(0);" onclick="responsive()">
+                <img alt="" class="menu" src="../media/burger.png">
+            </a>
+        </div>
+    </div>
+    <div class="right">
+        <ul class="nav">
+            <li class="nav-li">
+                <a href="../html/invest.html">Why Should You Invest?</a>
+            </li>
+            <li class="nav-li">
+                <a href="../html/zone.html">Investor Zone</a>
+            </li>
+            <li class="nav-li">
+                <a href="../html/economy.html">Statistics</a>
+            </li>
+            <li class="nav-li">
+                <a href="../html/innovate.html">Innovation</a>
+            </li>
+            <li class="nav-li">
+                <a href="../html/startup.html">Startup</a>
+            </li>
+            <li class="nav-li">
+                <a href="../html/contact.html">Contact</a>
+            </li>
+        </ul>
+    </div>
+    <div class="gradient"></div>
+</div>
+<script src="../scripts/main.js" type="text/javascript">
+</script>
+<script type="text/javascript">
+    window.onscroll = function () {
+        hideNav();
+    }
+</script>
+</body>
+</html>
 </body>
 </html>
