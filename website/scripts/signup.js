9330ef21dab025d3e908f617ff32172671cbad41
yasasgit <yasas99@outlook.com>
2021-06-21 16:03:16 +0530

    start up

--- website/scripts/signup.js
+++ website/scripts/signup.js
@@ -1,13 +1,12 @@
 function checkForm() {
     var firstname = document.form.firstname.value;
     var lastname = document.form.lastname.value;
-    var date = document.form.date.value;
-    var month = document.form.month.value;
-    var year = document.form.value;
+    var dob = document.form.dob.value;
     var gender = document.form.gender.value;
     var country = document.form.country.value;
     var email = document.form.email.value;
     var phone = document.form.phone.value;
+    var username = document.form.phone.value;
     var password = document.form.password.value;
     var confirmpassword = document.form.confirmpassword.value;
 
