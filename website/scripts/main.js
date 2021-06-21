79bc0fda5430cbc45cf680aeca1dd174749cfa37
yasasgit <yasas99@outlook.com>
2021-06-21 21:12:35 +0530

    news added to slides

--- website/scripts/main.js
+++ website/scripts/main.js
@@ -19,7 +19,7 @@ function showSlides() {
     }
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += " active";
-    setTimeout(showSlides, 5000);
+    setTimeout(showSlides, 8000);
 }
 
 function plusSlides(position) {
