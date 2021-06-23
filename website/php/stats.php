24ae9a2a7681b371dd1827425969a5ffcf621c5b
shaveen <shaveenmaleesha119@gmail.com>
2021-06-23 19:34:35 +0530

    add caption for other stats

--- website/php/stats.php
+++ website/php/stats.php
@@ -223,7 +223,8 @@ session_start();
     <div class="content" id="other">
         <div class="about">
             <h2>Other Statistics</h2>
-            <p>This data will show you why an investor should prefer Sri Lanka for their investments.</p>
+            <p>This data will show you why an investor should prefer Sri Lanka for their investments.
+            Provides statistical time series on socio economic and financial variables including provincially disaggregated data, where available. Released annually</p>
         </div>
     </div>
     <?php
